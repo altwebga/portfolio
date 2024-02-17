ebdb6916ad7cba9ab22f9826385aa7e9625c85db
Konstantin <altwebga@gmail.com>
2024-02-17 11:47:33 +0700

    Add: modal menu

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -2,19 +2,16 @@ import { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { RxActivityLog, RxCross1 } from "react-icons/rx";
 
-
 const NavModal = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const modalRef = useRef(); // Добавляем ссылку на модальное окно
+  const modalRef = useRef(); 
 
-  // Функция для закрытия модального окна при клике вне его
   const handleClickOutside = (event) => {
     if (modalRef.current && !modalRef.current.contains(event.target)) {
       setIsOpen(false);
     }
   };
 
-  // Добавляем и удаляем обработчик события клика
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
@@ -25,23 +22,38 @@ const NavModal = () => {
   return (
     <>
       <button
-        className="text-white flex flex-row items-center gap-2 ml-10"
+        className="text-white flex flex-row items-center gap-2"
         onClick={() => setIsOpen(!isOpen)}
       >
         <RxActivityLog />
         Меню
       </button>
       {isOpen && (
-        <div className="fixed inset-0 bg-black z-50 flex justify-center items-center w-full md:w-1/4" ref={modalRef}>
-            <button className="absolute mx-0" onClick={() => setIsOpen(false)}><RxCross1 className="text-white" /></button> {/* Кнопка закрытия */}
+        <div
+          className="fixed inset-0 bg-gray-900 z-50 flex justify-center items-center w-full md:w-1/4"
+          ref={modalRef}
+        >
+                   
           <nav className="text-white flex flex-col gap-4 text-2xl">
-            
-            <NavLink to="/" onClick={() => setIsOpen(false)}>Главная</NavLink>
-            <NavLink to="/about" onClick={() => setIsOpen(false)}>Обо мне</NavLink>
-            <NavLink to="/contact" onClick={() => setIsOpen(false)}>Контакты</NavLink>
-            <NavLink to="/services" onClick={() => setIsOpen(false)}>Услуги</NavLink>
-            <NavLink to="/blog" onClick={() => setIsOpen(false)}>Блог</NavLink>
+            <NavLink to="/" onClick={() => setIsOpen(false)}>
+              Главная
+            </NavLink>
+            <NavLink to="/about" onClick={() => setIsOpen(false)}>
+              Обо мне
+            </NavLink>
+            <NavLink to="/contact" onClick={() => setIsOpen(false)}>
+              Контакты
+            </NavLink>
+            <NavLink to="/services" onClick={() => setIsOpen(false)}>
+              Услуги
+            </NavLink>
+            <NavLink to="/blog" onClick={() => setIsOpen(false)}>
+              Блог
+            </NavLink>
           </nav>
+          <button className="absolute top-0 right-0 m-2" onClick={() => setIsOpen(false)}>
+            <RxCross1 className="text-white w-6 h-6" />
+          </button> 
         </div>
       )}
     </>
