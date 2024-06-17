ae9d2840665998d6a9cb8ebba7768a0acbd0db02
Konstantin <altwebga@gmail.com>
2024-06-17 20:38:27 +0700

    new project

--- src/app/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-const Portfolio = () => {
-    return (
-    <>
-    <h1>Portfolio</h1>
-    <p>text</p>
-    </>
-)
-}
-export default Portfolio;
--- src/config/menu.tsx
+++
@@ -1,9 +0,0 @@
-const HeaderMenu = [
-  { label: "Услуги", link: "/services" },
-  { label: "Обо мне", link: "/about" },
-  { label: "Портфолио", link: "/portfolio" },
-  { label: "Блог", link: "/blog" },
-  { label: "Контакты", link: "/contact" },
-];
-
-export default HeaderMenu;
