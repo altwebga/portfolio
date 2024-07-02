33efe2b1659589601a6f1ff204563680a6350e53
Konstantin <altwebga@gmail.com>
2024-07-02 18:25:04 +0700

    Add: bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,39 @@
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
+
+import { SpinningCube } from "@/components/SpinningCube";
 import { title, subtitle } from "@/components/primitives";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
+    <div className="bg-hero-grid bg-repeat bg-center h-full">
+      <div className="container mx-auto max-w-7xl px-4 py-6">
+        <div className="flex flex-col md:flex-row items-center md:pt-12">
+          <div className="flex-1">
+            <h1 className={title()}>Разработка и&nbsp;</h1>
+            <h1 className={title({ color: "violet" })}>продвижение&nbsp;</h1>
+            <br />
+            <h1 className={title()}>сайтов в Горно-Алтайске</h1>
+            <p className={subtitle({ class: "mt-4" })}>
+              Адекватный креатив и технологичные решения для вашего бизнеса.
+            </p>
+          </div>
+          <div className="flex-1">
+            <SpinningCube />
+          </div>
+        </div>
+        <div className="flex flex-row gap-4">
+          <Button as={Link} color="primary" href="/contact" variant="solid">
+            Начать проект
+          </Button>
+          <Button as={Link} color="default" href="/portfolio" variant="solid">
+            Примеры работ
+          </Button>
+        </div>
+        <div className="pt-12">
+          <h2 className={title({ size: "sm" })}>Весь спектр услуг</h2>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-import {nextui} from '@nextui-org/theme'
+// tailwind.config.js
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +14,13 @@ module.exports = {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-geist-mono)"],
       },
+      backgroundImage: {
+        "hero-pattern": "url('/image/hero-pattern.svg')",
+        "footer-texture": "url('/image/footer-texture.svg')",
+        "hero-grid": "url('/image/hero-grid.svg')",
+      },
     },
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
