18da540fc37e6fbe16edbd8931affed425fc541f
Konstantin <altwebga@gmail.com>
2026-07-12 15:02:38 +0300

    project cleanup

--- app/auth/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/components/shared/sign-up-form"
-
-export default function SignUpPage() {
-  return (
-    <main className="flex min-h-svh items-center justify-center p-6">
-      <SignupForm className="w-full max-w-sm" />
-    </main>
-  )
-}
